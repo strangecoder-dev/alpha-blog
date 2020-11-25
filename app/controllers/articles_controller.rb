35a7e1f5740de3476f44503ab398a2ea2f7a6336
strangecoder-dev <sushant@thewasserstoff.com>
2020-11-25 21:31:13 +0530

    Validation and Flash messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,20 @@ class ArticlesController < ApplicationController
     end
     
     def index
-        @article =Article.all
+        @articles =Article.all
+    end
+
+    def new
+        @article = Article.new
+    end
+
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        if @article.save
+            flash[:notice] = "Article was saved successfully"
+            redirect_to @article
+        else
+            render 'new'
+        end
     end
 end
